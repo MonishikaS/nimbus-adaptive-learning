da97b041034396f181c8dc4270d1b16f9d5cf5f7
MonishikaS <112560668+MonishikaS@users.noreply.github.com>
2025-11-09 16:03:22 +0530

    Delete backend directory

--- backend/src/routes/analytics.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { learnerMasteryTimeline } = require('../controllers/analytics.controller');
-const { authMiddleware } = require('../middleware/auth.middleware');
-
-router.get('/learner/:id/timeline', authMiddleware, learnerMasteryTimeline);
-
-module.exports = router;
--- backend/src/routes/quizzes.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { generateQuiz, recordAttempt } = require('../controllers/quizzes.controller');
-const { authMiddleware } = require('../middleware/auth.middleware');
-
-router.post('/generate', authMiddleware, generateQuiz);
-router.post('/attempt', authMiddleware, recordAttempt);
-
-module.exports = router;
