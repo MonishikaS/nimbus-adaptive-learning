71e231617b3e80ee0d89d01fc364fa1c42a4e7ef
MonishikaS <112560668+MonishikaS@users.noreply.github.com>
2025-11-09 15:59:11 +0530

    Create Welcome.jsx

---
+++ frontend/src/components/Welcome.jsx
@@ -0,0 +1,13 @@
+import "./Welcome.css";
+
+export default function Welcome() {
+  return (
+    <section className="welcome-section">
+      <h1>Welcome!</h1>
+      <p>
+        Ready to test your knowledge? Let's begin your personalized adaptive
+        learning journey.
+      </p>
+    </section>
+  );
+}
